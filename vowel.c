fc96a629cfd98dc773ca455e7d461fa06b66ead3
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-02 13:55:32 +0530

    vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char b,a;
+printf("enter the character");
+scanf("%c",&b);
+  a=b;
+if (a=='a' ||a=='e' ||a=='i '||a=='o' ||a=='u')
+{
+printf("the character is vowels");
+}
+else
+{
+printf("the character is consonant");
+}
+return 0;
+}
